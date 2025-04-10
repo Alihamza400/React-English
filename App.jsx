1b31e719cffb4863d64ced118d78c77b02950352
Ali Hamza <157949809+Alihamza400@users.noreply.github.com>
2025-04-10 19:45:33 +0500

    UseMemo Hook

--- App.jsx
+++ App.jsx
@@ -1,30 +1,39 @@
-import { useState,useEffect } from 'react'
-import  Component  from './Components.jsx'
-
-import './App.css'
+import { useState,useMemo } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(" ");
-  useEffect(()=>{
-    async function fetchData() {
-      const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&current_weather=true');
-      const data = await response.json();
-      const stringData = JSON.stringify(data);
-      setCount(stringData);
-    }
-    console.log(count);
-    fetchData();
 
-  },[]);
 
- 
+     const[counter,setcounter] = useState(0);
+     const[number,setnumber] = useState(0);
+    const expensive = (counter)=>{
+         for(let i=0;i<1000000000;i++){}
+          return counter;
+    
+  }
+  const increment = ()=>{
+    setcounter(counter+1);
+  }
+  const incre = ()=>{
+    setnumber(number+1);
+  }
+  
+  const data = useMemo(()=>
+    {
+      return expensive(counter);
+    },
+    [counter]
+    );
+
 
   return (
     <>
-       <h1>Fetched Data is : {count}</h1>
-       <h1>Count is : {count}</h1>
-       <Component/>
-
+    {/* {data} */}
+    <h1>Counter : {counter}</h1>
+    <button onClick={increment}>Counter</button>
+    <h1>Number: {number}</h1>
+    <button onClick={incre}>Number</button>
+      
     </>
   )
 }
